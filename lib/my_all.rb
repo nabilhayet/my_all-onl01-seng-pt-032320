5cdbfc070594404e6392c67015dba29c2692ee44
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-31 21:28:19 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
-def my_all?(collection)
-
+ddef my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+  
+  block_return_values.all? do |number|
+    
+  
+if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
